b22ff15ba0254fd85bde7c36fcf58ffbad2a135a
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-06-20 21:19:12 -0300

    Fibonacci e PA

--- ExercisesByClass/fibonacci.js
+++ ExercisesByClass/fibonacci.js
@@ -1,14 +1,33 @@
-let numA = 1
-let numB = 1
-let numC = 0
+// let numA = 1
+// let numB = 1
+// let numC = 0
 
-for(let i = 0; i <= 10; i++){
-    if(i == 0){
-        console.log(numA)
+// for(let i = 0; i <= 10; i++){
+//     if(i == 0){
+//         console.log(numA)
+//     }
+//     numC = numB 
+//     numB = numB + numA
+//     numA = numC
+//     console.log(numB)
+// }
+
+
+
+
+
+function fibonacci(num){
+    let a = 1
+    let b = 1
+    let c = 0
+    console.log(c)
+    console.log(a)
+    for(let i = 3; i <= num; i++){
+        a = a + c
+        c = b
+        b = a
+        console.log(a)
     }
-    numC = numB 
-    numB = numB + numA
-    numA = numC
-    console.log(numB)
 }
 
+fibonacci(10)
--- ExercisesByClass/pa.js
+++ ExercisesByClass/pa.js
@@ -1,10 +1,27 @@
-function progressaoaritmetica(num){
-    let pa = [];
-    for(let i = 1; i <= num; i++){
-        pa.includes(i)
+// function progressaoaritmetica(num){
+//     let pa = [];
+//     for(let i = 1; i <= num; i++){
+//         pa.includes(i)
+//     }
+//     return pa;
+// }
+
+// let resultado = progressaoaritmetica(8);
+// console.log(resultado)
+
+
+
+function pa(primeiroTermo, razao){
+    let soma = 0
+    for(let i = 0; i < 10; i++){
+        if(i == 0){
+            let termo = primeiroTermo
+            console.log(termo)
+        }else{
+            termo += razao
+            console.log(termo)
+        }
     }
-    return pa;
 }
 
-let resultado = progressaoaritmetica(8);
-console.log(resultado)
+pa(1,3)
