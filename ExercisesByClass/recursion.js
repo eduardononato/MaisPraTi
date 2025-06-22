2c8868bca774fbbb60afe23db42186b760764495
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-06-22 19:54:59 -0300

    Exercicio sobre recursividade

---
+++ ExercisesByClass/recursion.js
@@ -0,0 +1,55 @@
+// // function soma(n){
+// //     if(n === 1){
+// //         return 1   
+// //     }else{
+// //        return n + soma(n - 1)
+// //     }
+// // }
+
+// // console.log(soma(3))
+
+
+
+// let categorias = [
+//     {
+//         id: 1,
+//         nome: "Eletrônicos",
+//         filhos: [
+//             {id: 2, nome: "Celulares", filhos: []},
+//             {id: 3, nome: "Computadores", filhos: [
+//                 {id: 4, nome: "Tablets", filhos: []},
+//             ]}
+//         ]
+//     },
+//     {
+//         id: 5,
+//         nome: "Louças",
+//         filhos: []
+//     }
+// ]
+
+// obj(categorias)
+
+// function obj (cat){
+//     if(cat[0] !== null){
+//         console.log(cat[0].nome)
+//         if(cat[0].filhos !== null){
+//             cat[0].filhos[0]
+//         }
+//     }
+// }
+
+// soma dos números de 1 até n
+
+function sum(num){
+    if(num == 1){
+        return 1
+    }else{
+        return num + sum(num-1)
+    }
+}
+console.log(sum(10))
+
+// function fibonacci(n){
+//     if(n === 0)
+// }
