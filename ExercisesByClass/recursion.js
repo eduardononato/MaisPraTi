aecc117ac651916e1a874bbd7201ce53b44d6e9e
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-06-22 20:16:24 -0300

    Fibonacci recursivo

--- ExercisesByClass/recursion.js
+++ ExercisesByClass/recursion.js
@@ -41,15 +41,22 @@
 
 // soma dos números de 1 até n
 
-function sum(num){
-    if(num == 1){
+// function sum(num){
+//     if(num == 1){
+//         return 1
+//     }else{
+//         return num + sum(num-1)
+//     }
+// }
+// console.log(sum(10))
+
+function fibo(n){
+    if(n === 0){
+        return 0
+    }else if( n === 1 ){ 
         return 1
-    }else{
-        return num + sum(num-1)
+    }else{  
+        return fibo(n - 1) + fibo(n - 2)
     }
 }
-console.log(sum(10))
-
-// function fibonacci(n){
-//     if(n === 0)
-// }
+console.log(fibo(4))
