591dbdc6011421c0196f98ca6320e551998e1c2d
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-06-23 21:31:50 -0300

    Exercicio 2 lista de exercicios2

--- ListExercises02/exercicio2.js
+++ ListExercises02/exercicio2.js
@@ -3,8 +3,16 @@
 // tentativas e exibindo “mais alto” ou “mais baixo” a cada palpite errado
 
 const prompt = require('prompt-sync')(); 
-let inputNumber = Number(prompt('Digite um número aleatório entre 0 e 100: '));
-let randomNumber = 10
-do{
-    console.log('O número informado é menor do que o número sorteado')
-}while(randomNumber !== inputNumber)
+let randomNumber = Math.floor(Math.random() * 100) +1;
+let inputNumber = 0
+let counter = 0
+do{ 
+    counter ++
+    inputNumber= Number(prompt('Digite um número aleatório entre 0 e 100: '))
+    if(inputNumber < randomNumber){
+        console.log('mais alto')
+    }else if(inputNumber > randomNumber){
+        console.log('mais baixo')
+    }
+} while(randomNumber !== inputNumber)
+        console.log(`Você acertou em ${counter} tentativas`)
