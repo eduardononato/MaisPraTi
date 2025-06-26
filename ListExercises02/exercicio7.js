bfe795527d48480c7b7335574b13858be76dfba1
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-06-25 22:06:56 -0300

    feat: exercicio 07, retorna nomes ordenados por preço

--- ListExercises02/exercicio7.js
+++ ListExercises02/exercicio7.js
@@ -1,3 +1,18 @@
 // Dado um array produtos = [{ nome, preco }, ...], crie uma função que
 // retorne um novo array apenas com os nomes, ordenados por preço
 // crescente, usando map, sort.
+
+let myObj = [
+  { nome: "Banana", preco: 10.00 },
+  { nome: "Aveia", preco:  5.00 },
+  { nome: "Ovo", preco: 20.00 },
+  { nome: "Canela", preco: 2.50}
+];
+
+function orderByPrice(obj){
+    let newObj = obj.sort((a, b) => a.preco - b.preco)
+    let myMap = newObj.map(name => name.nome)
+    return myMap
+}
+
+console.log(orderByPrice(myObj))
