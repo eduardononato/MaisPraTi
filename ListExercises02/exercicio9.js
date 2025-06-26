faa962632ab2e2496c7d0a64d8b90c529785d58d
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-06-25 22:47:01 -0300

    feat: exercicio 09 - Convertendo ObjToArrya e ArrayToObj

--- ListExercises02/exercicio9.js
+++ ListExercises02/exercicio9.js
@@ -3,3 +3,26 @@
 // valor], ... ] e retorna o objeto equivalente.
 // ○ objetoParaPares(obj) faz o inverso, retornando um array de
 // pares.
+
+
+let myArray = [
+  ["nome", "Eduardo"],
+  ["idade", 26],
+  ["cidade", "São Bernardo do Campo"]
+]
+
+
+function paresParaObjeto(pares){
+    let myObj = Object.fromEntries(pares)
+    return myObj
+}
+
+myobj = paresParaObjeto(myArray)
+console.log(myobj)
+
+function objetoParaPares(obj){
+    let myArray = Object.entries(obj)
+    return myArray
+}
+
+console.log(objetoParaPares(myobj))
