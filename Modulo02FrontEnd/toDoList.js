f1602c1b6fed13c2ec9869dd7f057d1d8944b94a
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-07-16 21:04:10 -0300

    atualizando meu form

--- Modulo02FrontEnd/toDoList.js
+++ Modulo02FrontEnd/toDoList.js
@@ -5,7 +5,41 @@ const btnLimpar = document.getElementById('btnLimpar');
 
 let itens = [];
 
-formAdicionar.addEventListener(submit, (event) => {
+window.addEventListener('DOMContentLoaded', () => {
+    const dados = localStorage.getItem('listaCompras')
+    if(dados) {
+        itens = JSON.parse(dados)
+        renderizarLista()
+    }
+})
+
+function salvarDados() {
+    localStorage.setItem('listaCompras', JSON.stringify(itens))
+}
+
+function renderizarLista() {
+    listaItens.innerHTML = ''
+    itens.forEach((item, index) => {
+        const li = document.createElement('li')
+
+        const span = document.createElement('span')
+        span.textContent = item
+
+        const btnRemover = document.createElement('button')
+        btnRemover.textContent = 'Remover'
+        btnRemover.title = 'Remover Item'
+
+        btnRemover.addEventListener('click', () => {
+            removerItem(index)
+        })
+
+        li.append(span, btnRemover)
+        listaItens.appendChild(li)
+    })
+}
+
+formAdicionar.addEventListener('submit', (event) => {
+    event.preventDefault()
     const novoItem = inputItem.value.trim();
     if(!novoItem) {
         return
@@ -15,4 +49,20 @@ formAdicionar.addEventListener(submit, (event) => {
     renderizarLista()   
 
     inputItem.value = ''
-} )
+})
+
+function removerItem(indice) {
+    itens.splice(indice, 1)
+    salvarDados()
+    renderizarLista()
+}
+
+btnLimpar.addEventListener('click', () => {
+    if(confirm('Deseja limpar toda a lista?')){
+        itens = []
+        salvarDados()
+        renderizarLista()
+    }
+})
+
+
